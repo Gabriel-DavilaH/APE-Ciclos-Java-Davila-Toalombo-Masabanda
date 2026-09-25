c08c7b76ddb5c8bdd5e00b3a5d1569d82c25b30c
Gabriel Dávila <gabrieldavila1200@gmail.com>
2026-09-25 09:23:39 -0500

    Add files via upload

---
+++ DetectorCorregido.java
@@ -0,0 +1,10 @@
+public class DetectorCorregido {
+    public static void main(String[] args) {
+        int numero = 1;
+        while (numero <= 10) {
+            System.out.println(numero);
+            numero++; // <-- CORRECCION
+        }
+        System.out.println("Fin del programa");
+    }
+}
